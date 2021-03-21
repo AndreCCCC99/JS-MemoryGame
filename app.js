6096f4ccc9679ad407fd69a6fca974df0d184d85
AndreCCCC99 <andreclm99@gmail.com>
2021-03-21 22:54:37 +0000

    fixes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: 'SMOKER',
             img: 'images/smoker.PNG'
-        }
+        },
         {
             name: 'SMOKER',
             img: 'images/smoker.PNG'
