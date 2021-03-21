f5c121247e00e5cfd8248136a1a6bd5440a102ea
AndreCCCC99 <andreclm99@gmail.com>
2021-03-21 22:38:15 +0000

    functions: flipcard; checkMatches;

--- app.js
+++ app.js
@@ -34,23 +34,63 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'SMOKER',
             img: 'images/smoker.PNG'
         },
-    
+
     ]
 
     const grid = document.querySelector('.grid')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = [] 
 
+
+    //criar board 
     function createBoard() {
-        for(let i= 0; i < cardArray.length;i++ ){
-            var card = document.createElement('img');
-            card.setAttribute('src','images/blank.PNG');
-            card.setAttribute('data-id',i);
-
-            grid.appendChild(card);
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/pattern.jpg')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipcard)
+            grid.appendChild(card)
         }
     }
+
+    //check for match
+    function checkForMatch(){
+        var cards = document.querySelectorAll('img')
+        const optionOne = cardsChosenId[0];
+        const optionTwo = cardsChosenId[1];    
+        if (optionOne === optionTwo){
+            alert('match found!')
+            cards[optionOne].setAttribute('src','images/blank.PNG' )
+            cards[optionTwo].setAttribute('src','images/blank.PNG' )
+            cardsWon.push(cardsChosen)
+
+        }else{
+            cards[optionOne].setAttribute('src','images/pattern.jpg' )
+            cards[optionTwo].setAttribute('src','images/pattern.jpg' )
+        }
+    }
+
+
+    // flip card 
+
+    function flipcard() {
+
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length === 2){
+            setTimeout(checkForMatch,500)
+        }
+    }
+
 
 
 
     createBoard();
 
 })
+
+
+
